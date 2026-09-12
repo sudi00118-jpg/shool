d222f22157b9f65259179780b4fec182ea01a773
sudi00118-jpg <sudi00118@gmail.com>
2026-09-12 09:39:24 -0400

    Add Supabase configuration file

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+// supabase-config.js
+const SUPABASE_URL = 'https://bakigpmgiuoytjskmdfj.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_s8rK6dSkw_2ubaryRiqctw_OfZ8QoQF';
+
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
